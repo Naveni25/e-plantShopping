ba355a69978324869c44b628dde7377f8b7f9540
Naveni25 <128013030@sastra.ac.in>
2026-02-24 22:50:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
+    import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import CartItem from "./components/CartItem";
 
 function App() {
   
@@ -15,8 +20,30 @@ function App() {
   const handleHomeClick = () => {
     setShowProductList(false);
   };
-
   return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="landing-page">
+              <div className="landing-content">
+                <h1>Paradise Nursery</h1>
+                <button className="get-started-btn">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          }
+        />
+        <Route path="/plants" element={<ProductList />} />
+        <Route path="/cart" element={<CartItem />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
     <div className="app-container">
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
